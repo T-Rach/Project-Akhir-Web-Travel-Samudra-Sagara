53e0750a5d71e9faec5fcac4764b0a345882ca7e
NurulHikmahAskar221011111 <nh8973069@gmail.com>
2024-07-06 13:34:51 +0800

    Hikmah

--- code/register.php
+++ code/register.php
@@ -1,28 +1,50 @@
-<?php
-include 'db.php';
+<?php 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); 
-    $phone = isset($_POST['phone']) ? $_POST['phone'] : null; 
-    $address = isset($_POST['address']) ? $_POST['address'] : null; 
+include 'connect.php';
 
-    $sql = "INSERT INTO CUSTOMER (Nama, Email, Password, Customer_Mobile, Alamat, Username) VALUES (?, ?, ?, ?, ?, ?)";
+if(isset($_POST['register'])){
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $number_phone=$_POST['number_phone'];
+    $addres=$_POST['addres'];
 
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param("ssssss", $username, $email, $password, $phone, $address, $username);
-
-        if ($stmt->execute()) {
-            echo "Registration successful.";
-        } else {
-            echo "Error: " . $stmt->error;
-        }
-
-        $stmt->close();
-    } else {
-        echo "Error: " . $conn->error;
+    $checkEmail="SELECT * From user where email='$email'";
+    $result=$conn->query($checkEmail);
+    if($result->num_rows>0){
+        echo "Alamat Email Tidak Tersedia!";
     }
+    else{
+        $insertQuery="INSERT INTO user (username,email,password,number_phone, addres)
+                       VALUES ('$username','$email','$password','$number_phone', '$addres')";
+            if($conn->query($insertQuery)==TRUE){
+                header("location: index.php");
+            }
+            else{
+                echo "Error:".$conn->error;
+            }
+     }
+   
+
+}
+
+if(isset($_POST['login'])){
+   $email=$_POST['email'];
+   $password=$_POST['password'];
+   $password=($password) ;
+   
+   $sql="SELECT * FROM user WHERE email='$email' and password='$password'";
+   $result=$conn->query($sql);
+   if($result->num_rows>0){
+    session_start();
+    $row=$result->fetch_assoc();
+    $_SESSION['email']=$row['email'];
+    header("location: web.php");
+    exit();
+   }
+   else{
+    echo "Not Found, Incorrect Email or Password";
+   }
+
 }
 ?>
-
